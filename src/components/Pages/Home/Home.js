341c2e50bba16826e813e9de5784cbd29720b4f6
Progna_Labony_Roy <labony.progna@gmail.com>
2022-12-06 16:03:52 +0600

    home-page edited

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home</h1>
+    <div className="mx-auto">
+      <h1>Welcome to our website CSE Basic</h1>
+      <br />
+      <h3>Explore courses and Read blogs free</h3>
     </div>
   );
 };
